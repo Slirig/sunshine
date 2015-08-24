ac11c7beeb1c8809ce08518925f136f898a86bd0
slirig <slirig88@gmail.com>
2015-08-24 17:55:44 +0200

    new project

---
+++ app/src/main/java/org/example/sunshine/PlaceholderFragment.java
@@ -0,0 +1,21 @@
+package org.example.sunshine;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Master on 2015-08-24.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
